49d61db62eb38d83139aa84f2fbe0196e50674d5
xuefeng xu <119409007+jiayue13@users.noreply.github.com>
2025-11-11 22:47:38 +0800

    Update run_fastp.sh

--- run_fastp.sh
+++ run_fastp.sh
@@ -10,7 +10,7 @@ SAMPLES=("ProQ_1" "ProQ_2" "ProQ_3" "WT_1" "WT_2" "WT_3")
 for SAMPLE in "${SAMPLES[@]}"; do
     echo "🧼 Running fastp for $SAMPLE..."
     fastp -i $indir/${SAMPLE}_R1.fastq.gz -I $FASTQ_DIR/${SAMPLE}_R2.fastq.gz \
-          -o ${SAMPLE}.R1.raw.fastq.gz -O ${SAMPLE}.R1.raw.fastq.gz \
+          -o ${SAMPLE}.R1.raw.fastq.gz -O ${SAMPLE}.R2.raw.fastq.gz \
           -h ${SAMPLE}_fastp.html -j ${SAMPLE}_fastp.json \
           -q 20 -u 30 -n 5 -l 50
 done
